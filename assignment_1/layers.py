c35ef2b12b907c8a7ea57501fdfa55c2b35580c5
Diwas Sharma <diwasblack@gmail.com>
2018-06-14 15:56:05 -0500

    Added code for backpropagation
    - Uses entire training data at once
    - Uses L2 norm as cost meaure in final layer

--- assignment_1/layers.py
+++ assignment_1/layers.py
@@ -18,18 +18,23 @@ class LayerBase():
         self.output_units = output_units
 
         # Randomly initialize weights in the range [-0.5, 0.5]
-        self.weights = -0.5 + np.random.rand(self.output_units, self.input_units)
+        self.weights = -0.5 + \
+            np.random.rand(self.output_units, self.input_units)
 
         self.activation_function = None
         self.activation_function_derivative = None
 
-        # Store the activation value calcuated during the forward pass
+        # Store the input values during the forward_pass
+        self.input_values = None
+        # Store the activation value calculated during the forward pass
         self.activation_values = None
 
     def forward_pass(self, input_matrix):
         """
         Layer method to compute the activation values during forward pass
         """
+        self.input_values = np.copy(input_matrix)
+
         # Compute the linear combination of weight and layer input
         linear_combination = np.dot(self.weights, input_matrix)
 
@@ -38,8 +43,24 @@ class LayerBase():
 
         return self.activation_values
 
-    def backward_pass(self):
-        pass
+    def update_weights(self, weight_update):
+        """
+        Update the weight of the layer
+        """
+
+        self.weights = self.weights - weight_update
+
+    def backward_pass(self, delta):
+        """
+        Layer method to calculate the delta updates from next layer delta
+        """
+
+        derivative_values = self.activation_function_derivative(
+            self.activation_values)
+
+        dloss_dz = np.multiply(delta, derivative_values)
+
+        return dloss_dz
 
 
 class TanhLayer(LayerBase):
--- assignment_1/sequential_nn.py
+++ assignment_1/sequential_nn.py
@@ -1,5 +1,6 @@
-import numpy as np
+import logging
 
+import numpy as np
 
 from layers import TanhLayer
 
@@ -12,8 +13,8 @@ class SequentialNeuralNet():
     def __init__(self, input_dimension):
         # List to hold the layers
         self.layers = []
-
         self.input_dimension = input_dimension
+        self.learning_rate = None
 
     def add_layer(self, activation_function="tanh", units=64, use_softmax=False):
         if(not(self.layers)):
@@ -27,27 +28,69 @@ class SequentialNeuralNet():
         # Add layer to the list
         self.layers.append(layer)
 
-    def compile(self):
-        self.output_dimension = self.layers[-1].output_dimension
+    def compile(self, learning_rate=0.001, error_threshold=0.001):
+        self.output_dimension = self.layers[-1].output_units
+        self.learning_rate = learning_rate
 
-    def predict(self, input_matrix):
-        output = input_matrix
+        self.error_threshold = error_threshold
+
+        self.number_of_layers = len(self.layers)
+
+    def forward_pass(self, input_matrix):
+        output = np.copy(input_matrix)
         for layer in self.layers:
             output = layer.forward_pass(output)
 
         return output
 
+    def backpropagation(self, input_matrix, predicted_output, target_matrix):
+        eta = self.learning_rate
 
-if __name__ == "__main__":
-    input_data_dimension = 3
-    number_of_samples = 10
-    nn_object = SequentialNeuralNet(input_dimension=input_data_dimension)
-    nn_object.add_layer(units=32)
-    nn_object.add_layer(units=64)
-    nn_object.add_layer(use_softmax=True)
+        # Assumes using the L2 distance for cost
+        dloss_dy = predicted_output - target_matrix
 
-    input_data = np.random.rand(input_data_dimension, 10)
-    output = nn_object.predict(input_data)
+        delta = np.dot(self.layers[-1].weights.T, dloss_dy)
 
-    import pdb
-    pdb.set_trace()
+        # Weight update for final layer
+        weight_update = eta * \
+            np.dot(dloss_dy, self.layers[-1].input_values.T)
+
+        self.layers[-1].update_weights(weight_update)
+
+        for layer_index in range(self.number_of_layers-2, -1, -1):
+
+            # Access layer object
+            layer = self.layers[layer_index]
+
+            # Propagate delta through layers
+            dloss_dz = layer.backward_pass(delta)
+
+            # Calculate delta before weight update
+            delta = np.dot(layer.weights.T, dloss_dz)
+
+            # Update weight of the layer
+            weight_update = eta * np.dot(dloss_dz, layer.input_values.T)
+            layer.update_weights(weight_update)
+
+    def train(self, input_matrix, target_matrix):
+        number_of_iterations = 0
+        while(True):
+            # Propagate the input forward
+            predicted_output = self.forward_pass(input_matrix)
+
+            # Calculate the loss
+            loss = np.linalg.norm(predicted_output-target_matrix)
+
+            if(loss < self.error_threshold):
+                break
+
+            if(number_of_iterations % 1000 == 0):
+                logging.info("Cost: {}".format(loss))
+
+            # Update weights using backpropagation
+            self.backpropagation(input_matrix, predicted_output, target_matrix)
+
+            number_of_iterations += 1
+
+    def predict(self, input_matrix):
+        return self.forward_pass(input_matrix)
