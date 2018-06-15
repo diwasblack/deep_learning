05f0093f8848447205a045f298969c5878293157
Diwas Sharma <diwasblack@gmail.com>
2018-06-15 08:53:15 -0500

    Fix for mean_squared_error loss function

--- assignment_1/loss_functions.py
+++ assignment_1/loss_functions.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 
-def mean_squared_error(target_matrix, predicted_output):
+def mean_squared_error(predicted_output, target_matrix):
     delta = predicted_output - target_matrix
-    return np.linalg.norm(delta)
+    squared_errors = np.sum(np.square(delta), axis=0)
+    return np.mean(squared_errors)
