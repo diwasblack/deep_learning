55ab001f179bb4f32dcd3eacc5f6338978da5c58
Diwas Sharma <diwasblack@gmail.com>
2018-06-21 11:48:32 -0500

    Using preprocessed CIFAR10 dataset

--- assignment_1/cifar10_nn_train.py
+++ assignment_1/cifar10_nn_train.py
@@ -2,15 +2,21 @@ import logging
 
 from cifar10 import CIFAR10NN
 
-from cifar10_dataset import load as load_dataset
+from cifar10_dataset import get_CIFAR10_data
 
 
 def train_mnist():
     logging.basicConfig(
         format='%(asctime)s %(levelname)s %(message)s', level=logging.DEBUG)
 
-    # Load dataset for CIFAR 10
-    x_train, y_train, x_test, y_test = load_dataset('cifar-10-batches-py')
+    # Load preprocessed dataset for CIFAR10
+    cifar10_dataset= get_CIFAR10_data('cifar-10-batches-py')
+
+    x_train = cifar10_dataset['X_train']
+    y_train = cifar10_dataset['y_train']
+
+    x_test = cifar10_dataset['X_test']
+    y_test = cifar10_dataset['y_test']
 
     # Flatten and transform x_train and x_test
     x_train = x_train.reshape(x_train.shape[0], -1).T
